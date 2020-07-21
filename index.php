006de54284e12149f93f76f09dd26f0aee0e040f
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2020-07-20 22:16:06 -0300

    Fundo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+
+<body class="fundo">
+
+</body>
+
+</html>
